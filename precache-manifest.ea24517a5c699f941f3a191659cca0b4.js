8cd7a63a9af829d179131f8ee2bf684065b88fa0
AsadShah2003 <car30114059@gmail.com>
2023-07-09 18:21:42 +0500

    Updates

---
+++ precache-manifest.ea24517a5c699f941f3a191659cca0b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b338e45759ac6058acef",
+    "url": "/AsadShah2003/react-app-movie-info/static/css/main.f60e6ee0.chunk.css"
+  },
+  {
+    "revision": "b338e45759ac6058acef",
+    "url": "/AsadShah2003/react-app-movie-info/static/js/main.cdb12066.chunk.js"
+  },
+  {
+    "revision": "ceb385b6395a81a94b39",
+    "url": "/AsadShah2003/react-app-movie-info/static/js/runtime~main.4ba5054a.js"
+  },
+  {
+    "revision": "d47691cb562f4a94dd31",
+    "url": "/AsadShah2003/react-app-movie-info/static/js/2.85506ffe.chunk.js"
+  },
+  {
+    "revision": "3976a6b7fa6bb4d6d871c3dffd374228",
+    "url": "/AsadShah2003/react-app-movie-info/index.html"
+  }
+];
